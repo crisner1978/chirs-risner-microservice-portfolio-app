c9f8bed337091c227eaeec597499fcf2f7053978
crisner1978 <crisner.prsvr@gmail.com>
2021-09-01 12:23:27 -0500

    Add mongoose, mongodb, bodyparser, nanoid to package.json Update server.js for short url Update index page to display link for shorturl microservice Add page for shorturl microservice

--- server.js
+++ server.js
@@ -3,8 +3,19 @@
 
 // init project
 var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+//may use Nano Id instead
+const { nanoid } = require('nanoid')
+// var shortid = require('shortid')
 var app = express();
 var port = process.env.PORT || 3000
+require('dotenv').config();
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true, useUnifiedTopology: true
+});
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -28,6 +39,10 @@ app.get("/requestHeaderParser", function (req, res) {
   res.sendFile(__dirname + '/views/requestHeaderParser.html');
 });
 
+app.get("/urlShortener", function (req, res) {
+  res.sendFile(__dirname + '/views/urlShortener.html');
+});
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
@@ -35,6 +50,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//Timestamp get request
 app.get("/api/timestamp", function(req, res) {
   let now = new Date()
   res.json({
@@ -58,16 +74,62 @@ app.get("/api/timestamp/:date_string", function (req, res) {
   }  
 })
 
+//Request Header Parser get request
 app.get("/api/whoami", function (req, res) {
   res.json({
     "ipaddress": req.headers['x-forward-for'] || req.socket.remoteAddress || null,
     "language": req.headers["accept-language"],
-    "software": req.headers["user-agent"]
-    
+    "software": req.headers["user-agent"]    
   })
 })
 
+//URL Shortener
 
+
+//Build schema and model to save URLS
+const ShortURL = mongoose.model('ShortURL', new mongoose.Schema({ 
+  short_url: String,
+  original_url: String,
+  suffix: String
+}));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// create application/json parser
+
+app.post("/api/shorturl/new/", function (req, res) {
+
+  let suffix = nanoid(8)
+  let requestedURL = req.body.url
+  let newShortURL = suffix
+
+  let newURL = new ShortURL ({
+    short_url: __dirname + "/api/shorturl/" + suffix,
+    original_url: requestedURL,
+    suffix: suffix
+  })
+
+  newURL.save(function(err, doc) {
+    if (err) return console.error(err);
+    res.json({
+    "saved": true,
+    "short_url": newURL.short_url,
+    "original_url": newURL.original_url,
+    "suffix": newURL.suffix
+    });
+  });
+})
+
+app.get("/api/shorturl/:suffix", function(req, res) {
+  let generatedSuffix = req.params.suffix;
+  ShortURL.find({suffix: generatedSuffix}).then(function(foundUrls) {
+    let urlRedirect = foundUrls[0];
+    res.redirect(urlRedirect.original_url)
+  })
+});
 
 
 // listen for requests :)
